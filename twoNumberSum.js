f095f49436da401156d69b44a488d0cc270d48a7
Peter Oehman <tucker2923@gmail.com>
2021-12-23 11:38:38 -0500

    two number sum

---
+++ twoNumberSum.js
@@ -0,0 +1,8 @@
+function twoNumberSum(array, targetSum) {
+	for (let i = 0; i < array.length; i++) {
+		for (let j = 0; j < array.length; j++) {
+      if (array[i] + array[j] === targetSum && i !== j) return [array[i], array[j]];
+    };
+  };
+  return [];
+};
